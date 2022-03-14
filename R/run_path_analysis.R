3b4bc0972486193afd667ea5dc5c43ff81ca9480
DEliza <32403146+Elizaddh@users.noreply.github.com>
2022-03-14 17:00:16 -0400

    Update run_path_analysis.R

--- R/run_path_analysis.R
+++ R/run_path_analysis.R
@@ -2,7 +2,7 @@
 #' @export
 
 run_path_analysis<- function(lf.c, gsets ){
-    fc.kegg.p <- gage(lf.c, gsets , ref = NULL, samp = NULL,compare = "unpaired")
+    fc.kegg.p <- gage(lf.c, gsets , ref = NULL, samp = NULL)
     
  
     #return(path.ids.2)
