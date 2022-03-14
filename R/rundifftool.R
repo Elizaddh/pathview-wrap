5eb451bfd4a8541dc977f075863dd446bf7b67a5
DEliza <32403146+Elizaddh@users.noreply.github.com>
2022-03-14 18:16:06 -0400

    Update rundifftool.R

--- R/rundifftool.R
+++ R/rundifftool.R
@@ -21,9 +21,9 @@ rundifftool <- function(diff.tool, gene.data, ref, samp){
         deseq2.fc=deseq2.res$log2FoldChange
         names(deseq2.fc)=rownames(deseq2.res)
         exp.fc=deseq2.fc
-        pdf("Volcano_deseq2.pdf", width = 4,height= 4)
-        #EnhancedVolcano(deseq2.res, x = 'log2FoldChange', y = 'pvalue', lab = rownames(deseq2.res))
-        plot(x = 1:10, y = 1:10)
+        pdf("Volcano_deseq2.pdf", width = 14,height= 14)
+        EnhancedVolcano(deseq2.res, x = 'log2FoldChange', y = 'pvalue', lab = rownames(deseq2.res))
+        #plot(x = 1:10, y = 1:10)
         dev.off()
         print("this is the png file")
    }
