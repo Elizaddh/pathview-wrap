6071ff0beab623605357e19856c1904c97329983
DEliza <32403146+Elizaddh@users.noreply.github.com>
2022-03-16 22:03:01 -0400

    Update run_path_analysis.R

--- R/run_path_analysis.R
+++ R/run_path_analysis.R
@@ -1,11 +1,8 @@
 #' This is the function to run pathway analysis
 #' @import gage
+#'
 #' @export
-
 run_path_analysis<- function(lf.c, gsets ){
-    fc.kegg.p <- gage(lf.c, gsets , ref = NULL, samp = NULL)
-    
- 
-    #return(path.ids.2)
+    fc.kegg.p <- gage::gage(lf.c, gsets , ref = NULL, samp = NULL)
     return(fc.kegg.p)
 }
