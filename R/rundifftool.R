8efe311af237b1940cb697bb9d53acdf959b370b
DEliza <32403146+Elizaddh@users.noreply.github.com>
2022-03-16 22:02:25 -0400

    Update rundifftool.R

--- R/rundifftool.R
+++ R/rundifftool.R
@@ -3,11 +3,10 @@
 #' @import edgeR
 #' @import limma
 #' @import EnhancedVolcano
-
+#'
 #' @export
- 
 rundifftool <- function(diff.tool, gene.data, ref, samp, outname){
- library(EnhancedVolcano)
+ 
     grp.idx = NULL
     grp.idx[ref] <- "reference"
     grp.idx[samp] <- "sample"
@@ -15,11 +14,11 @@ rundifftool <- function(diff.tool, gene.data, ref, samp, outname){
     {
    if (diff.tool == "deseq2")
     {
-        library(DESeq2)
+        
         print("Deseq2 is running")
-        coldat=DataFrame(grp=factor(grp.idx))
-        dds <- DESeqDataSetFromMatrix(gene.data, colData=coldat, design = ~ grp)
-        dds <- DESeq(dds)
+        coldat=DESeq2::DataFrame(grp=factor(grp.idx))
+        dds <- DESeq2::DESeqDataSetFromMatrix(gene.data, colData=coldat, design = ~ grp)
+        dds <- DESeq2::DESeq(dds)
         deseq2.res <- results(dds)
         #direction of fc, depends on levels(coldat$grp), the first level
         #taken as reference (or control) and the second one as experiment.
@@ -28,38 +27,38 @@ rundifftool <- function(diff.tool, gene.data, ref, samp, outname){
         exp.fc=deseq2.fc
         #pdf("Volcano_deseq2.pdf", width = 14,height= 14)
         tiff(paste0(outname,"Volcano_deseq2.tiff"), units="in", width=15, height=15, res=300)
-        plot(EnhancedVolcano(deseq2.res, x = 'log2FoldChange', y = 'pvalue', lab = rownames(deseq2.res)))
+        plot(EnhancedVolcano::EnhancedVolcano(deseq2.res, x = 'log2FoldChange', y = 'pvalue', lab = rownames(deseq2.res)))
         #plot(x = 1:10, y = 1:10)
         dev.off()
         print("this is the png file")
    }
     if (diff.tool == "edgeR"){
-    library(edgeR)
-    dgel <- DGEList(counts=gene.data, group=factor(grp.idx))
-    dgel <- calcNormFactors(dgel)
-    dgel <- estimateCommonDisp(dgel)
-    dgel <- estimateTagwiseDisp(dgel)
-    et <- exactTest(dgel)
+    
+    dgel <- edgeR::DGEList(counts=gene.data, group=factor(grp.idx))
+    dgel <- edgeR::calcNormFactors(dgel)
+    dgel <- edgeR::estimateCommonDisp(dgel)
+    dgel <- edgeR::estimateTagwiseDisp(dgel)
+    et <- edgeR::exactTest(dgel)
     edger.fc=et$table$logFC
     names(edger.fc)=rownames(et$table)
     exp.fc=edger.fc
     tiff(paste0(outname,"Volcano_edgeR.tiff"), units="in", width=15, height=15, res=300)
-    plot(EnhancedVolcano(et$table, x = 'logFC', y = "PValue", lab = rownames(et$table)))
+    plot(EnhancedVolcano::EnhancedVolcano(et$table, x = 'logFC', y = "PValue", lab = rownames(et$table)))
      dev.off()
     
     }
     if(diff.tool == "limma")
         {
-        library(edgeR)
+        
        
-       dgel2 <- DGEList(counts=gene.data, group=factor(grp.idx))
-       dgel2 <- calcNormFactors(dgel2)
+       dgel2 <- edgeR::DGEList(counts=gene.data, group=factor(grp.idx))
+       dgel2 <- edgeR::calcNormFactors(dgel2)
        library(limma)
-       design <- model.matrix(~grp.idx)
-       log2.cpm <- voom(dgel2,design)
-       fit <- lmFit(log2.cpm,design)
-       fit <- eBayes(fit)
-       limma.res=topTable(fit,coef=2,n=Inf,sort="p")
+       design <- limma::model.matrix(~grp.idx)
+       log2.cpm <- limma::voom(dgel2,design)
+       fit <- limma::lmFit(log2.cpm,design)
+       fit <- limma::eBayes(fit)
+       limma.res=limma::topTable(fit,coef=2,n=Inf,sort="p")
        limma.fc=limma.res$logFC
  
        names(limma.fc)=limma.res$ID
