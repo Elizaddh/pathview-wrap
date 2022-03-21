f8db9e3476f8a82782519907d6c40b3979c0fe34
DEliza <32403146+Elizaddh@users.noreply.github.com>
2022-03-21 16:38:50 -0400

    Update pathview.2.R
    bug fix, stringr lib

--- R/pathview.2.R
+++ R/pathview.2.R
@@ -26,7 +26,7 @@ pathview.2 <- function( run ,diff.tool, gene.data= cnts, ref, samp, gsets, pathw
         path.ids.l <- rownames(fc.kegg.p$less)[fc.kegg.p$less[, "q.val"] < 0.1 &
                                                  + !is.na(fc.kegg.p$less[,"q.val"])]
         #path.ids.2 <- substr(c(path.ids.2[1:3], path.ids.l[1:3]), 1, 8)
-        path.ids2 <- gsub("[^0-9.-]", "", sapply(str_split(path.ids.2, " ", 2),"[[",1))
+        path.ids2 <- gsub("[^0-9.-]", "", sapply(stringr::str_split(path.ids.2, " ", 2),"[[",1))
       }
       #visualize pathway  
       pv.out.list <- sapply(na.omit(path.ids.2[1:6]), function(pid) pathview::pathview( gene.data = logfoldchange, 
@@ -42,7 +42,7 @@ pathview.2 <- function( run ,diff.tool, gene.data= cnts, ref, samp, gsets, pathw
         path.ids.l <- rownames(fc.kegg.p$less)[fc.kegg.p$less[, "q.val"] < 0.1 &
                                                  + !is.na(fc.kegg.p$less[,"q.val"])]
         #path.ids.2 <- substr(c(path.ids.2[1:3], path.ids.l[1:3]), 1, 8)
-        path.ids2 <- gsub("[^0-9.-]", "", sapply(str_split(path.ids.2, " ", 2),"[[",1))
+        path.ids2 <- gsub("[^0-9.-]", "", sapply(stringr::str_split(path.ids.2, " ", 2),"[[",1))
         
         #visualize pathway  
         pv.out.list <- sapply(na.omit(path.ids.2[1:6]), function(pid) pathview::pathview( gene.data =  gene.data, 
