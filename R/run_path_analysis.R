edb374276364e851691cd33075656658c0cb2dc8
DEliza <32403146+Elizaddh@users.noreply.github.com>
2022-03-21 16:45:37 -0400

    Update run_path_analysis.R

--- R/run_path_analysis.R
+++ R/run_path_analysis.R
@@ -2,7 +2,7 @@
 #' @import gage
 #'
 #' @export
-run_path_analysis<- function(lf.c, gsets ){
-    fc.kegg.p <- gage::gage(lf.c, gsets , ref = NULL, samp = NULL)
+run_path_analysis<- function(lf.c, gsets, ref = ref, samp = samp, compare ){
+    fc.kegg.p <- gage::gage(lf.c, gsets , ref = NULL, samp = NULL,compare)
     return(fc.kegg.p)
 }
